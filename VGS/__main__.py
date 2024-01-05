34b5fcee59c0d38111c0fdc42c94be83e357b290
Axe Botz <136898915+Axe-Botz@users.noreply.github.com>
2024-01-05 21:34:36 +0530

    Create __main__.py

---
+++ VGS/__main__.py
@@ -0,0 +1,19 @@
+import asyncio
+import importlib
+from pyrogram import Client, idle
+
+from VGS import clients, app, ids
+
+async def start_bot():
+    for cli in clients:
+        try:
+            await cli.start()
+            ex = await cli.get_me()            
+            print(f"Started {ex.first_name} 🔥")
+            ids.append(ex.id)
+        except Exception as e:
+            print(f"{e}")
+    await idle()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
