f2186e7edf2d7130f7d3770c703bfe9647cb82bf
Axe Botz <136898915+Axe-Botz@users.noreply.github.com>
2024-01-06 19:24:11 +0530

    Update __main__.py

--- VGS/__main__.py
+++ VGS/__main__.py
@@ -2,27 +2,16 @@ import asyncio
 import importlib
 from pyrogram import Client, idle
 
-from VGS import clients, ids
-
-
-async def join(client):
-    try:
-        await client.join_chat("AxeBotz")
-    except BaseException:
-        pass
+from VGS import client
 
 
 async def start_bot():
-    for cli in clients:
-        try:
-            await cli.start()
-            ex = await cli.get_me()
-            await join(cli)
-            print(f"Started {ex.first_name} 🔥")
-            #ids.append(ex.id)
-        except Exception as e:
-            print(f"{e}")
-    await idle()
+    try:
+        print("WTF! L o a d i n g")
+        await client.start()
+    except:
+        print("Error!")
+    
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(start_bot())
