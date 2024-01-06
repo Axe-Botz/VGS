eda12320a749248fbe11c9aca22f8f2e5cf67039
Axe Botz <136898915+Axe-Botz@users.noreply.github.com>
2024-01-06 23:36:53 +0530

    Update __main__.py

--- VGS/__main__.py
+++ VGS/__main__.py
@@ -1,19 +1,6 @@
-import asyncio
-import importlib
-from pyrogram import Client, idle
-
-from VGS import userbot
-
-loop = asyncio.get_event_loop()
-
-async def start_bot():
-    try:
-        print("WTF! L o a d i n g")
-        await userbot.start()
-    except Exception as e:
-        print(f"{e}")
-    
+from . import *
 
 if __name__ == "__main__":
-    loop.run_until_complete(start_bot())
+    Run_SpamX()
+    
 print("[INFO]: BOT STARTED 🔥")
